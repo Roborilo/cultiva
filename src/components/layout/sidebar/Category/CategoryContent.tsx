ac6026d3603a6ead40049e803688d5fcc03f3c46
matheus-kiyoshi <matheus.kiyoshi.f@gmail.com>
2023-11-20 09:28:00 -0300

    fix: deselect category if clicked again

--- src/components/layout/sidebar/Category/CategoryContent.tsx
+++ src/components/layout/sidebar/Category/CategoryContent.tsx
@@ -19,7 +19,7 @@ export default function CategoryContent({ name, quantity }: ICategoryProps) {
 
 	return (
 		<button className="w-full flex justify-between items-center text-white font-medium text-2xl hover:underline border-b border-gray-600 pb-4" onClick={() => {
-			setCategory(name)
+			setCategory(category === name ? null : name)
 		}}>
 			<p>{name}</p>
 			<p>{quantity}</p>
